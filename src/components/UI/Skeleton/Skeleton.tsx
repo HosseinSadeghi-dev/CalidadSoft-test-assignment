9e57bc9cb59ede97f929297687d84b0aa0b62311
Hossein Sadeghi <sadeghihossein.hs@gmail.com>
2025-01-15 02:33:49 +0330

    feat: add Skeleton component for loading placeholders

---
+++ src/components/UI/Skeleton/Skeleton.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import styles from "./skeleton.module.scss";
+
+interface Props {
+  className?: string;
+}
+
+const Skeleton: React.FC<Props> = ({ className }) => {
+  return (
+    <div
+      className={`bg-zinc-300 relative overflow-hidden rounded-lg ${className}`}
+    >
+      <div
+        className={`${styles.skeleton} absolute top-0 right-0 bottom-0 left-0`}
+      ></div>
+    </div>
+  );
+};
+
+export default Skeleton;
