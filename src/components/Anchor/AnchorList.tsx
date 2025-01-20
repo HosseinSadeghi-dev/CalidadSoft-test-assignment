98b2ab5ea850c3c578f67a308b57331dd3b41699
Hossein Sadeghi <sadeghihossein.hs@gmail.com>
2025-01-20 12:33:35 +0330

    feat: add selection highlighting to AnchorList component
    - Introduced anchorIsSelected function to determine if an anchor is currently selected based on the window location hash.
    - Updated AnchorList to apply conditional styling for selected anchors, enhancing user navigation experience.
    - Improved accessibility and visual feedback for users interacting with the anchor list.

--- src/components/Anchor/AnchorList.tsx
+++ src/components/Anchor/AnchorList.tsx
@@ -9,6 +9,13 @@ interface Props {
 const AnchorList: React.FC<Props> = ({ className }) => {
   const anchors: TOCAnchor[] = useAnchorStore((state) => state.anchors);
 
+  const anchorIsSelected = (anchor: string): boolean => {
+    console.log("anchor", anchor.toLowerCase());
+    console.log("window.location.hash", window.location.hash);
+
+    return anchor.toLowerCase() === window.location.hash;
+  };
+
   return (
     <nav className={`flex flex-col justify-start items-stretch ${className}`}>
       {anchors.length > 0 && (
@@ -16,10 +23,17 @@ const AnchorList: React.FC<Props> = ({ className }) => {
           <b className="mb-4 dark:text-white text-sm">On this page: </b>
           <ul className="flex flex-col justify-start items-stretch text-sm">
             {anchors.map((anchor) => (
-              <li key={anchor.id} className="text-black dark:text-white">
+              <li
+                key={anchor.id}
+                className={`text-black dark:text-white border-l hover:bg-[#19191C0D] dark:hover:bg-[#252527] ${
+                  anchorIsSelected(anchor.anchor)
+                    ? "border-l-[#6B57FF] border-l-[3px]"
+                    : "border-l-[#19191C33] dark:border-l-[#252527]"
+                } pl-2`}
+              >
                 <a
                   href={`${anchor.anchor.toLowerCase()}`}
-                  className="hover:bg-blue-300 flex-1 py-2"
+                  className="flex-1 py-2"
                 >
                   {anchor.title}
                 </a>
